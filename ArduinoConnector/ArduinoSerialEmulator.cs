187f0b9ccd4cbb47cbcb4f0984a979a132629268
Jamie Thompson <jamie.thompson.1024@gmail.com>
2023-07-15 15:41:15 +1200

    Change to Impliment Connection Interface

--- ArduinoConnector/ArduinoSerialEmulator.cs
+++ ArduinoConnector/ArduinoSerialEmulator.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace ArduinoConnector
 {
-    public class ArduinoEmulator : IArduinoConnection
+    public class ArduinoSerialEmulator : IArduinoConnection
     {
         public List<(string, string)> MessageHistory => throw new NotImplementedException();
 
