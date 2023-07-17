19d1bf5e0c2b22a80702b247624965974383a5ef
Jamie Thompson <jamie.thompson.1024@gmail.com>
2023-07-17 18:50:23 +1200

    Used AutoResetEvent to wait on arduino reponse instead of using events

--- ArduinoConnector/ArduinoConnector.cs
+++ ArduinoConnector/ArduinoConnector.cs
@@ -2,11 +2,15 @@
 {
     public class ArduinoConnector : IArduinoConnector
     {
-        public event EventHandler<TestPinConnectionsMessageEventArgs> TestPinConnectionsMessage;
-        public event EventHandler<DeviceTypeMessageEventArgs> DeviceTypeMessage;
+        //public event EventHandler<TestPinConnectionsMessageEventArgs> TestPinConnectionsMessage;
+        //public event EventHandler<DeviceTypeMessageEventArgs> DeviceTypeMessage;
         public event EventHandler<ErrorMessageEventArgs> ErrorMessage;
+        public event EventHandler<LogMessageEventArgs> LogMessage;
 
         IArduinoConnection _connection;
+        AutoResetEvent _autoResetEvent;
+
+        string[] _responseArgs;
 
         public ArduinoConnector(IArduinoConnection connection)
         {
@@ -17,28 +21,41 @@
         private void MessageReceivedHandler(object sender, ArduinoMessageReceivedEventArgs e)
         {
             string rawMessage = e.Message;
-            List<string> commandArguments = new List<string>(rawMessage.Split(' '));
-            string command = commandArguments[0];
-            commandArguments.RemoveAt(0);
+            string[] commandArguments = rawMessage.Split(' ');
 
-            switch (command)
+            _responseArgs = commandArguments;
+
+            if (_responseArgs[0].Equals("Error"))
+            {
+                ErrorMessage(
+                    this,
+                    new ErrorMessageEventArgs(_responseArgs[1])
+                );
+            }
+
+            _autoResetEvent.Set();
+
+            /*
+            switch (commandArguments[0])
             {
                 case "DeviceType":
-                    OnDeviceTypeMessage(commandArguments.ToArray());
+                    //OnDeviceTypeMessage(commandArguments.ToArray());
                     break;
                 case "TestPinConnectionsResults":
-                    OnTestPinConnectionsMessage(commandArguments.ToArray());
+                    //OnTestPinConnectionsMessage(commandArguments.ToArray());
                     break;
                 case "Error":
-                    OnErrorMessage(commandArguments.ToArray());
+                    //OnErrorMessage(commandArguments.ToArray());
                     break;
             }
+            */
         }
 
+        /*
         private void OnDeviceTypeMessage(string[] arguments)
         {
             string deviceType = arguments[0];
-            DeviceTypeMessage(this, new DeviceTypeMessageEventArgs(deviceType));
+            //DeviceTypeMessage(this, new DeviceTypeMessageEventArgs(deviceType));
         }
 
         private void OnErrorMessage(string[] arguments)
@@ -60,15 +77,50 @@
                 );
             }
 
-            TestPinConnectionsMessage(
-                this,
-                new TestPinConnectionsMessageEventArgs(pin, testedPins)
-            );
+            //TestPinConnectionsMessage(
+            //    this,
+            //    new TestPinConnectionsMessageEventArgs(pin, testedPins)
+            //);
         }
+        */
 
-        public void TestPinConnections(int pin, int[] testPins)
+        public int[] TestPinConnections(int pin, int[] testPins)
         {
             _connection.SendMessage($"TestPinConnections {pin} {String.Join(",", testPins)}");
+            _autoResetEvent.WaitOne(500);
+
+            if (_responseArgs == null)
+            {
+                throw new TimeoutException("Timedout Waiting for Response from Arduino");
+            }
+
+            if (_responseArgs[0].Equals("Error"))
+            {
+                throw new Exception($"Error {_responseArgs[1]}");
+            }
+
+            if (!_responseArgs[0].Equals("TestPinConnectionsResults"))
+            {
+                throw new Exception($"Unexpected Response, Expected 'TestPinConnectionsResults' Got '{_responseArgs[0]}'");
+            }
+
+            if (int.Parse(_responseArgs[1]) != pin)
+            {
+                throw new Exception("Unexpected Pin Number on Response");
+            }
+
+            int[] testedPins = { };
+
+            if (!_responseArgs[2].Equals("N/C"))
+            {
+                testedPins = Array.ConvertAll<string, int>(
+                    _responseArgs[2].Split(','),
+                    new Converter<string, int>(x => int.Parse(x))
+                );
+            }
+
+            _responseArgs = null;
+            return testedPins;
         }
 
         public void SetPinOutput(int pin, bool state)
@@ -76,9 +128,39 @@
             _connection.SendMessage($"SetPinOutput {pin} {(state ? "1" : "0")}");
         }
 
-        public void GetDeviceType()
+        public string GetDeviceType()
         {
             _connection.SendMessage("GetDeviceType");
+            _autoResetEvent.WaitOne(500);
+
+            if (_responseArgs == null)
+            {
+                throw new TimeoutException("Timedout Waiting for Response from Arduino");
+            }
+
+            if (_responseArgs[0].Equals("Error"))
+            {
+                throw new Exception($"Error {_responseArgs[1]}");
+            }
+
+            if (!_responseArgs[0].Equals("TestPinConnectionsResults"))
+            {
+                throw new Exception($"Unexpected Response, Expected 'DeviceType' Got '{_responseArgs[0]}'");
+            }
+
+            string deviceType = _responseArgs[1];
+            _responseArgs = null;
+            return deviceType;
+        }
+
+        public void OpenConnection()
+        {
+            _connection.OpenConnection();
+        }
+
+        public void CloseConnection()
+        {
+            _connection.CloseConnection();
         }
     }
 }
