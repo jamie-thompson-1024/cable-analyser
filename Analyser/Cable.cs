720a55960f5f796fc135490afe3260c9e5683c7f
Jamie Thompson <jamie.thompson.1024@gmail.com>
2023-08-09 20:57:16 +1200

    h

--- Analyser/Cable.cs
+++ Analyser/Cable.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DeviceConnector;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -14,21 +15,27 @@ namespace CableAnalyser
         
         }
 
+        public static Cable GenerateFromScan(IDeviceConnector connector)
+        {
+
+        }
+
         public (PortType, int)[] GetPorts()
         {
             List<(PortType, int)> ports = new List<(PortType, int)>();
             foreach(Connection connection in _connections)
             {
-                (PortType, int) port = 
 
-                if (!ports.Contains((connection.PinA.PortType, connection.PinA.PortNumber)))
+                (PortType, int) portA = (connection.PinA.PortType, connection.PinA.PortNumber);
+                if (!ports.Contains(portA))
                 {
-                    ports.Add()
+                    ports.Add(portA);
                 }
 
+                (PortType, int) portB = (connection.PinB.PortType, connection.PinB.PortNumber);
                 if (!ports.Contains((connection.PinB.PortType, connection.PinB.PortNumber)))
                 {
-
+                    ports.Add(portB);
                 }
             }
         }
